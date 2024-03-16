06d9803837e7124002dafc925d9f167d588cb685
Wahab Alshahin <wahab.alshahin@gmail.com>
2024-03-16 15:53:59 -0400

    Create base package

---
+++ example/jaspr_html_convert_example.dart
@@ -0,0 +1,6 @@
+import 'package:jaspr_html_convert/jaspr_html_convert.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/jaspr_html_convert_test.dart
@@ -0,0 +1,16 @@
+import 'package:jaspr_html_convert/jaspr_html_convert.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
